2e5c47685ff2dec5a200d5d2bb96e8e7229aa8a6
Konst_VB <varjutin@gmail.com>
2025-04-23 23:39:20 +0300

    fix imports

--- src/feature/sideBar/ui/app-sidebar.tsx
+++ src/feature/sideBar/ui/app-sidebar.tsx
@@ -25,8 +25,8 @@ import {
 } from "@/entities/department/types";
 import useStoreUser from "@/entities/user/store/useStoreUser";
 import { UserResponse } from "@/entities/user/types";
-import { NavMain } from "@/feature/sideBar/ui/nav-main";
-import { NavUser } from "@/feature/sideBar/ui/nav-user";
+import { NavMain } from "./nav-main";
+import { NavUser } from "./nav-user";
 
 const icons = {
   SALES: <BadgeRussianRuble />,
