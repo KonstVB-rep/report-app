11772e1aed98708d2de98e216d37f649a85a7a88
Konst_VB <varjutin@gmail.com>
2025-05-19 13:17:45 +0300

    refactoring login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,8 +33,6 @@ export default function LoginPage() {
     }
   }, [isAuth, authUser, router]);
 
-  if (isAuth) return <Loading />;
-
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <Image
