da4a33155477691af75249c75b0682c61e63cc6a
Konst_VB <varjutin@gmail.com>
2025-05-21 07:51:47 +0300

    change Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-"use client";
+import { redirect } from 'next/navigation'
 
-import { useEffect } from "react";
-
-import { useRouter } from "next/navigation";
-
-const TemplateDashboard = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/login")
-  }, [router]);
-  
-
-  return (
-    <div className="relative grid h-full place-items-center p-4">
-    </div>
-  );
-};
-
-export default TemplateDashboard;
+export default function Home() {
+  redirect('/login')
+}
