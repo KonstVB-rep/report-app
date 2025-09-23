c48f142d834fd7313854febf2571dda1d58357a5
Konst_VB <varjutin@gmail.com>
2025-09-23 17:41:29 +0300

    fix animate style

--- src/shared/custom-components/ui/MotionComponents/MotionDivY.tsx
+++ src/shared/custom-components/ui/MotionComponents/MotionDivY.tsx
@@ -5,12 +5,14 @@ import { cn } from "@/shared/lib/utils";
 const MotionDivY = ({
   children,
   className = "",
+  keyValue,
 }: {
   children: React.ReactNode;
   className?: string;
+  keyValue?: string | number;
 }) => {
   return (
-      <div className={cn("animate-slide-appear", className)}>{children}</div>
+      <div key={keyValue} className={cn("animate-slide-appear", className)}>{children}</div>
   );
 };
 
