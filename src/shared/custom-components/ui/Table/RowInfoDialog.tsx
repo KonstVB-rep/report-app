cca86baff4c48bf5c5bbc8fa33101c7968bbe770
Konst_VB <varjutin@gmail.com>
2026-04-14 09:45:28 +0300

    fix style

--- src/shared/custom-components/ui/Table/RowInfoDialog.tsx
+++ src/shared/custom-components/ui/Table/RowInfoDialog.tsx
@@ -25,10 +25,10 @@ const RowInfoDialog = ({ text, isActive, closeFn, isTargetCell, children }: Prop
         createPortal(
           <div className="absolute inset-0 w-full h-full bg-black/40 z-50">
             <div
-              className="absolute left-1/2 w-full max-h-[80vh] w-[max(96vw, 280px)] sm:max-w-[400px] top-1/2 -translate-x-1/2 -translate-y-1/2 h-auto p-5 bg-primary-foreground border-2 shadow-lg rounded-md z-10 text-primary"
+              className="absolute left-1/2 w-full w-[max(96vw, 280px)] sm:max-w-[600px] top-1/2 -translate-x-1/2 -translate-y-1/2 h-auto p-5 bg-primary-foreground border-2 shadow-lg rounded-md z-10 text-primary"
               ref={ref}
             >
-              <span className="block text-base text-center md:text-lg p-2 bg-background rounded-md">
+              <span className="block text-base text-center md:text-lg p-2 bg-background rounded-md max-h-[80vh] overflow-y-auto">
                 {text}
               </span>
               {isTargetCell && children}
